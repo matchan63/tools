5e8af80ab3a1546a26cb19f72378fa7ba921a3ae
Matt Chan <matt@matchan.net>
2020-03-09 10:39:31 +1100

    added comments

--- test/nm1b.py
+++ test/nm1b.py
@@ -1,6 +1,7 @@
 from netmiko import ConnectHandler 
 from getpass import getpass
 
+#also add a comment
 connection = ConnectHandler(ip="10.1.1.251", device_type="cisco_ios", username="admin", password=getpass())
 
 print (connection.send_command("sh ip int brief | ex una"))
