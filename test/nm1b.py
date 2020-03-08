ffd61897354092353a01bb61304b07698fe25573
Matt Chan <matt@matchan.net>
2020-03-09 10:49:46 +1100

    more comment changes testing git

--- test/nm1b.py
+++ test/nm1b.py
@@ -1,9 +1,11 @@
 from netmiko import ConnectHandler 
 from getpass import getpass
 
-#also add a comment
+#I added a comment
 connection = ConnectHandler(ip="10.1.1.251", device_type="cisco_ios", username="admin", password=getpass())
 
+#added another comment to check changes in git
 print (connection.send_command("sh ip int brief | ex una"))
 
+#another comment to check changes in git
 connection.disconnect()
