991829f1eb7762d5accc7b256458adf51ce34df7
Matt Chan <matt@matchan.net>
2020-04-22 10:00:07 +1000

    tunnel tool base v2 input paramaters from file

--- test/grebuild2.py
+++ test/grebuild2.py
@@ -21,60 +21,79 @@
 from netmiko import ConnectHandler
 from getpass import getpass
 import re
+import pdb
 
 def file_template():
     return
 
-#get tunnel and device info
-with open("tunnel1.txt") as infile:
-    output = infile.read().splitlines()
-
-password = getpass()
-
+def tunnel_gre():
 #build device login profiles
-dev1  = {'ip'          : re.search(r"^.*:(.*)", output[1]).group(1),
-         'device_type' : re.search(r"^.*:(.*)", output[2]).group(1),
-         'username'    : re.search(r"^.*:(.*)", output[0]).group(1),
-         'password'    : password
-          }
+    password = getpass()
 
-dev2  = {'ip'          : re.search(r"^.*:(.*)", output[5]).group(1),
-         'device_type' : re.search(r"^.*:(.*)", output[6]).group(1),
-         'username'    : re.search(r"^.*:(.*)", output[0]).group(1),
-         'password'    : password
-          }
+    dev1  = {'ip'          : re.search(r"^.*:(.*)", output[2]).group(1),
+             'device_type' : re.search(r"^.*:(.*)", output[3]).group(1),
+             'username'    : re.search(r"^.*:(.*)", output[1]).group(1),
+             'password'    : password
+              }
 
-connection = ConnectHandler(**dev1)
-print("Building 'A' end....")
+    dev2  = {'ip'          : re.search(r"^.*:(.*)", output[6]).group(1),
+             'device_type' : re.search(r"^.*:(.*)", output[7]).group(1),
+             'username'    : re.search(r"^.*:(.*)", output[1]).group(1),
+             'password'    : password
+              }
 
-cmd = ['interface tunnel0',
-       'ip address ' + re.search(r"^.*:(.*)", output[3]).group(1),
-       'tunnel source ' + re.search(r"^.*:(.*)", output[1]).group(1),
-       'tunnel destination ' + re.search(r"^.*:(.*)", output[5]).group(1),
-       'ip route ' + re.search(r"^.*:(.*)", output[8]).group(1) + ' tunnel0']
-result = connection.send_config_set(cmd)
-print("-" * 80)
-print(result)
-print("-" * 80)
+#    pdb.set_trace()
+    connection = ConnectHandler(**dev1)
+    print("Building 'A' end....")
 
-connection.disconnect()
-print("'A' end complete")
+    cmd = ['interface tunnel0',
+           'ip address ' + re.search(r"^.*:(.*)", output[4]).group(1),
+           'tunnel source ' + re.search(r"^.*:(.*)", output[2]).group(1),
+           'tunnel destination ' + re.search(r"^.*:(.*)", output[6]).group(1),
+           'ip route ' + re.search(r"^.*:(.*)", output[9]).group(1) + ' tunnel0']
+    result = connection.send_config_set(cmd)
+    print("-" * 80)
+    print(result)
+    print("-" * 80)
 
-connection = ConnectHandler(**dev2)
-print("Building 'B' end....")
+    connection.disconnect()
+    print("'A' end complete")
 
-cmd = ['interface tunnel0',
-       'ip address ' + re.search(r"^.*:(.*)", output[7]).group(1),
-       'tunnel source ' + re.search(r"^.*:(.*)", output[5]).group(1),
-       'tunnel destination ' + re.search(r"^.*:(.*)", output[1]).group(1),
-       'ip route ' + re.search(r"^.*:(.*)", output[4]).group(1) + ' tunnel0']
-result = connection.send_config_set(cmd)
-print("-" * 80)
-print(result)
-print("-" * 80)
+    connection = ConnectHandler(**dev2)
+    print("Building 'B' end....")
 
-connection.disconnect()
-print("'B' end complete")
+    cmd = ['interface tunnel0',
+           'ip address ' + re.search(r"^.*:(.*)", output[8]).group(1),
+           'tunnel source ' + re.search(r"^.*:(.*)", output[6]).group(1),
+           'tunnel destination ' + re.search(r"^.*:(.*)", output[2]).group(1),
+           'ip route ' + re.search(r"^.*:(.*)", output[5]).group(1) + ' tunnel0']
+    result = connection.send_config_set(cmd)
+    print("-" * 80)
+    print(result)
+    print("-" * 80)
 
-print('''\n\nTunnel build complete, please test connectivity then save config\n
+    connection.disconnect()
+    print("'B' end complete")
+
+def tunnel_ipsec():
+    return
+
+
+#get tunnel and device info
+try:
+    with open("tunnel.txt") as infile:
+        output = infile.read().splitlines()
+    if 'tunnel_type:gre' in output:
+        tunnel_gre()
+    elif 'tunnel_type:ipsec' in output:
+        tunnel_ipsec()
+    else:
+        print("Invalid tunnel type, must be 'gre' or 'ipsec'")
+        exit()
+
+    print('''\n\nTunnel build complete, please test connectivity then save config\n
         on DevA and DevB''')
+
+except FileNotFoundError:
+    print("File does not exist, template file created...")
+    exit()
