aaa5b8ae60bb7cadc72578af042253b48195005b
Matt Chan <matt@matchan.net>
2020-05-04 18:01:21 +1000

    add base_config tool

--- test/base_config.py
+++ test/base_config.py
@@ -2,20 +2,19 @@
 import yaml
 import jinja2
 from pprint import pprint
+from getpass import getpass
 
 filename = "base_config.yml"
 with open(filename) as f:
-    active_interfaces = yaml.full_load(f)
+    initial_config = yaml.full_load(f)
 
 
-template_file = "base_config.j2"
+template_file = "base_configa.j2"
 with open(template_file) as f:
-    interface_template = f.read()
+    config_template = f.read()
 
-print("active_interfaces")
-pprint(active_interfaces)
-print("\nactive_interfaces['interfaces']")
-pprint(active_interfaces['interfaces'])
+initial_config['password'] = getpass()
+pprint(initial_config)
 
-template = jinja2.Template(interface_template)
-print(template.render(active_interfaces = active_interfaces['interfaces']))
+template = jinja2.Template(config_template)
+print(template.render(initial_config))
